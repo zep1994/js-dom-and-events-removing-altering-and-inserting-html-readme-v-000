4b49b239dd3eaeaf0ddfcecd0f0237f78182756a
zep1994 <snaxbags@protonmail.com>
2018-10-31 02:10:32 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,26 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+var element = document.createElement('div');
+
+element.innerHTML = 'Hello, DOM!';
+element.style.backgroundColor = '#f9f9f9';
+
+document.body.appendChild(element);
+
+element.style.textAlign = 'center';
+
+var ul = document.createElement('ul');
+
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li');
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(li);
+}
+
+element.appendChild(ul);
+
+ul.style.textAlign = 'left';
+
+ul.removeChild(ul.querySelector('li:nth-child(2)'));
+
+ul.remove();
